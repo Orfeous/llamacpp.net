73b29350906437a6cc667f8c1eb9721ace4bbe10
Gabor Racz <orfeous.b@gmail.com>
2023-06-17 20:39:58 +0100

    Create SafeLLamaContextHandle.cs

---
+++ src/LlamaCpp.Net/Native/SafeLLamaContextHandle.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace LlamaCpp.Net.Native
+{
+    /// <summary>
+    /// todo
+    /// </summary>
+    internal class SafeLLamaContextHandle : SafeLLamaHandleBase
+    {
+        /// <summary>
+        /// todo
+        /// </summary>
+        internal SafeLLamaContextHandle() { }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="handle"></param>
+        /// <param name="ownsHandle"></param>
+        internal SafeLLamaContextHandle(IntPtr handle, bool ownsHandle) : base(handle, ownsHandle) { }
+
+        /// <summary>
+        /// todo
+        /// </summary>
+        /// <param name="handle"></param>
+        internal SafeLLamaContextHandle(IntPtr handle) : base(handle) { }
+
+        /// <summary>
+        /// todo
+        /// </summary>
+        /// <returns></returns>
+        protected override bool ReleaseHandle()
+        {
+            LlamaNative.llama_free(handle);
+            SetHandle(IntPtr.Zero);
+            return true;
+        }
+    }
+}
