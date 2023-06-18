0feec9164a0fe73f47c5e1e2eb3a0548ffef435d
Marcel <marcelrlof@gmail.com>
2023-06-18 20:39:42 +0200

    move constants to separate file

--- src/LlamaCpp.Net.Test/LanguageModelTests.cs
+++ src/LlamaCpp.Net.Test/LanguageModelTests.cs
@@ -65,25 +65,3 @@ namespace LlamaCpp.Net.Test
         }
     }
 }
-
-public static class Constants
-{
-    public static string ModelDirectory => Path.Join(GetRepositoryRoot(), "models");
-
-    private static string GetRepositoryRoot()
-    {
-        var baseDirectory = AppDomain.CurrentDomain.BaseDirectory;
-
-
-        while (!Directory.Exists(Path.Join(baseDirectory, ".git")))
-        {
-            var directoryInfo = new DirectoryInfo(baseDirectory).Parent;
-            if (directoryInfo != null)
-            {
-                baseDirectory = directoryInfo.FullName;
-            }
-        }
-
-        return baseDirectory;
-    }
-}
