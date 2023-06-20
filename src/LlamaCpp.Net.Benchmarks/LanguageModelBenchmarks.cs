6546947895e71dd206da2c3b4cee16e2d5ab92e0
Marcel <marcelrlof@gmail.com>
2023-06-20 21:01:47 +0200

    added basic benchmarks

---
+++ src/LlamaCpp.Net.Benchmarks/LanguageModelBenchmarks.cs
@@ -0,0 +1,51 @@
+﻿using BenchmarkDotNet.Attributes;
+
+namespace LlamaCpp.Net.Benchmarks;
+
+public class LanguageModelBenchmarks
+{
+    private readonly LanguageModel languageModel;
+
+    public LanguageModelBenchmarks()
+    {
+        this.languageModel = new LanguageModel("models/117M", null);
+        var fileName = "wizardLM-7B.ggmlv3.q4_0.bin";
+
+        var modelPath = Path.Join(Constants.ModelDirectory, fileName);
+
+        this.languageModel = new LanguageModel(modelPath, null);
+    }
+
+    [Benchmark]
+    public void Tokenize()
+    {
+        var input = "This is a test";
+
+        var tokens = this.languageModel.Tokenize(input);
+    }
+
+    [Benchmark]
+    public void TokenToString()
+    {
+        var input = "This is a test";
+
+        var tokens = this.languageModel.Tokenize(input);
+
+        foreach (var token in tokens)
+        {
+            this.languageModel.TokenToString(token);
+        }
+    }
+
+    [Benchmark]
+    public void Infer()
+    {
+        var input = "This is a test";
+
+
+        this.languageModel.Infer(input);
+    }
+
+
+
+}
